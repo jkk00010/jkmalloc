4442f187d1cf5d275b8e55aa0d457d4effd11e4e
Jakob Kaivo <jkk@ung.org>
2022-11-20 15:53:42 -0500

    go all the way to a page under

--- test/underflow.c
+++ test/underflow.c
@@ -8,7 +8,7 @@ int main(void)
 	char *ptr = malloc(1);
 	long pagesize = sysconf(_SC_PAGESIZE);
 	printf("ptr: %p, pagesize %ld\n", (void*)ptr, pagesize);
-	for (int i = 0; i < pagesize; i++) {
+	for (int i = 0; i <= pagesize; i++) {
 		ptr[-i] = '\0';
 	}
 	printf("shouldn't get here\n");
